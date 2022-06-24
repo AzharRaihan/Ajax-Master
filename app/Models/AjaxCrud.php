f940b56d31cf426506d01d6422548d8bcf0cf63f
Azhar Raihan <azharraihan6969@gmail.com>
2022-06-25 00:34:00 +0600

    ajax crud complete

--- app/Models/AjaxCrud.php
+++ app/Models/AjaxCrud.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class InfoCollect extends Model
+class AjaxCrud extends Model
 {
     use HasFactory;
 
